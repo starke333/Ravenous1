1f6574df06f078348b5229cd926dbd0518be8d7d
starke333 <pettereric@gmail.com>
2017-11-27 22:48:09 +0100

    first commit

---
+++ src/util/Yelp.js
@@ -0,0 +1,46 @@
+const clientId = 'TF1djMp7Dz9odHd7RjvcOg';
+const secret = 'THFhhvnMJ5kltYG79u0NDNUXD0pzyDVo0FunzHgTDZqS0Fnc48Et1mHTHuUXxXcn';
+let accessToken;
+
+const Yelp = {
+getAccessToken(){
+  if(accessToken){
+    return new Promise(resolve => resolve(accessToken));
+  }
+  return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/oauth2/token?grant_type=client_credentials&client_id=${clientId}&client_secret=${secret}`, {
+    method: 'POST'
+  }).then(response => {
+     return response.json();
+   }).then(jsonResponse => {
+     accessToken= jsonResponse.access_token;
+   });
+},
+
+search(term, location, sortBy){
+  return Yelp.getAccessToken().then(() => {
+     return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+       headers: {
+         Authorization: `Bearer ${accessToken}`
+       }
+     });
+   }).then(response => {
+        return response.json();
+      }).then(jsonResponse =>{
+        if(jsonResponse.businesses){
+  return jsonResponse.businesses.map(business => ({
+    id: business.id,
+    imageSrc: business.image_url,
+    name: business.name,
+    address: business.location.address1,
+    city: business.location.city,
+    state: business.location.state,
+    zipCode: business.location.zip_code,
+    category: business.categories[0].title,
+    rating: business.rating,
+    reviewCount: business.review_count
+        }));
+      }
+    });
+  }
+};
+export default Yelp;
